4e355caaeffc7ab7e10a915ea618bf4e39076837
StathisPlakakis <stathisplakorg1997@gmail.com>
2024-07-06 19:23:25 +0300

    Add  testCases for hit method

--- src/Classes/ship.test.js
+++ src/Classes/ship.test.js
@@ -6,20 +6,36 @@ test('Ship Class initialization', () => {
       received: 2,
       expectedSize: 2,
       expectedTotalHits: 0,
-      expectedtoBeSunk: false
-      ,
+      expectedtoBeSunk: false,
     },
     {
       received: 12,
       expectedSize: 12,
       expectedTotalHits: 0,
-      expectedtoBeSunk: false
-      ,
-    }
+      expectedtoBeSunk: false,
+    },
   ];
   testCases.forEach((testCase) => {
     expect(new Ship(testCase.received).size).toBe(testCase.expectedSize);
-    expect(new Ship(testCase.received).totalHits).toBe(testCase.expectedTotalHits);
+    expect(new Ship(testCase.received).totalHits).toBe(
+      testCase.expectedTotalHits
+    );
     expect(new Ship(testCase.received).isSunk).toBe(testCase.expectedtoBeSunk);
+  });
+});
+
+test('Ship Class Method Hit', () => {
+  const testCases = [
+    {
+      received: 2,
+      expectedTotalHits: 2,
+    },
+  ];
+  testCases.forEach((testCase) => {
+    const ship = new Ship();
+    for (let i = 0; i < testCase.received; i++) {
+      ship.hit();
+    }
+    expect(ship.totalHits).toBe(testCase.expectedTotalHits);
   });
 });
