8f50879911f7526c383b49798e252f7d11885525
StathisPlakakis <stathisplakorg1997@gmail.com>
2024-07-08 18:03:08 +0300

    Add the testcase for randomizeShipsPosition method
    This test is quite more verbose,it tries to explain how our method works

--- src/Classes/gameboard.test.js
+++ src/Classes/gameboard.test.js
@@ -1,4 +1,4 @@
-import Gameboard from './gameboard.js'
+import Gameboard from './gameboard.js';
 
 test('Gameboard Class Initialization', () => {
   const testCases = [
@@ -15,10 +15,39 @@ test('Gameboard Class Initialization', () => {
         [[], [], [], [], [], [], [], [], [], []],
         [[], [], [], [], [], [], [], [], [], []],
         [[], [], [], [], [], [], [], [], [], []],
-      ]
+      ],
     },
   ];
   testCases.forEach((testCase) => {
-    expect(new Gameboard(testCase.received).board).toEqual(testCase.expected);
+    expect(new Gameboard().board).toEqual(testCase.expected);
+  });
+});
+
+test('Random Placement of ships', () => {
+  const testCases = [
+    {
+      receivedNumberOfShips: [1], //the number of elements in the array indicates the total number of ships,while the exact number in each index indicates the size of the respected ship
+
+      expextedCompositionOfActiveSquares: { 0: 1 }, //active squares are the squares, where ships have been placed and each square has the index value of the ship that represents
+    },
+  ];
+  testCases.forEach((testCase) => {
+    const newBoard = new Gameboard();
+    newBoard.randomizeShipsPosition(testCase.receivedNumberOfShips); // this is the method we are trying to test!This method, randomizes the position of the ships we want and overall changes the result of this.board
+    const result = {};
+    for (let i = 0; i < newBoard.board.length; i++) {
+      for (let j = 0; j < newBoard.board[i].length; j++) {
+        const specificCell = newBoard.board[i][j];
+        //We itterate through all the cells of the board! If the cell is not empty, we take the value of the cell (which represents a part of SPECIFIC ship) and we increment the times we have seen the ship
+        if (specificCell.length !== 0) {
+          if (result[specificCell] === undefined) {
+            result[specificCell] = 1;
+          } else {
+            result[specificCell] += 1;
+          }
+        }
+      }
+    }
+    expect(result).toEqual(testCase.expextedCompositionOfActiveSquares);
   });
 });
