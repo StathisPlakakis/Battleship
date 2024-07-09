73175fc55b8450149b4bdf61f7c3300f16997ef7
StathisPlakakis <stathisplakorg1997@gmail.com>
2024-07-09 13:19:40 +0300

    Add one more method to make randomizeShipsPosition smaller

--- src/Classes/gameboard.js
+++ src/Classes/gameboard.js
@@ -14,63 +14,69 @@ class Gameboard {
     ];
   }
 
+  pickRandomStartingPoint(lengthOfShip, direction) {
+    let randomiseIsDone = false;
+    while (!randomiseIsDone) {
+      //we check if the cell that contains the starting point of the ship is empty and inside the boundaries of our board
+      const x_axis = Math.floor(Math.random() * 10);
+      const y_axis = Math.floor(Math.random() * 10);
+      let successfulLoopIterration = false;
+      for (let i = 0; i < lengthOfShip; i++) {
+        if (direction === 'horizontal') {
+          if (
+            x_axis + i > 9 ||
+            this.board[x_axis + i][y_axis].length !== 0 //this checks if all the cells, that are going to contain our ship, are valid
+          ) {
+            successfulLoopIterration = false;
+            break;
+          } else {
+            successfulLoopIterration = true;
+          }
+        }
+        if (successfulLoopIterration) {
+          for (let i = 0; i < lengthOfShip; i++) {
+            randomiseIsDone = true;
+          }
+          return [x_axis, y_axis];
+        } else {
+          if (
+            y_axis - i < 0 ||
+            this.board[x_axis][y_axis - i].length !== 0 //this checks if all the cells, that are going to contain our ship, are valid
+          ) {
+            successfulLoopIterration = false;
+            break;
+          } else {
+            successfulLoopIterration = true;
+          }
+        }
+        if (successfulLoopIterration) {
+          for (let i = 0; i < lengthOfShip; i++) {
+            randomiseIsDone = true;
+          }
+          return [x_axis, y_axis];
+        }
+      }
+    }
+  }
+
   randomizeShipsPosition(array) {
     for (let ship = 0; ship < array.length; ship++) {
       const directions = ['horizontal', 'vertical'];
       const randomDirection = Math.floor(Math.random() * 2);
       const direction = directions[randomDirection];
+      const startingPoint = this.pickRandomStartingPoint(
+        array[ship],
+        direction
+      );
+      const x_axis = startingPoint[0];
+      const y_axis = startingPoint[1];
       if (direction === 'horizontal') {
-        let randomiseIsDone = false;
-        while (!randomiseIsDone) {
-          //we check if the cell that contains the starting point of the ship is empty and inside the boundaries of our board
-          const x_axis = Math.floor(Math.random() * 10);
-          const y_axis = Math.floor(Math.random() * 10);
-          const sizeOfShip = array[ship];
-          let successfulLoopIterration = false;
-          for (let i = 0; i < sizeOfShip; i++) {
-            if (
-              x_axis + i > 9 ||
-              this.board[x_axis + i][y_axis].length !== 0 //this checks if all the cells, that are going to contain our ship, are valid
-            ) {
-              successfulLoopIterration = false;
-              break;
-            } else {
-              successfulLoopIterration = true;
-            }
-          }
-          if (successfulLoopIterration) {
-            for (let i = 0; i < sizeOfShip; i++) {
-              this.board[x_axis + i][y_axis] = ship;
-            }
-            randomiseIsDone = true;
-          }
+        for (let i = 0; i < array[ship]; i++) {
+          this.board[x_axis + i][y_axis] = ship;
         }
-      }
-      if (direction === 'vertical') {
-        let randomiseIsDone = false;
-        while (!randomiseIsDone) {
-          //we check if the cell that contains the starting point of the ship is empty and inside the boundaries of our board
-          const x_axis = Math.floor(Math.random() * 10);
-          const y_axis = Math.floor(Math.random() * 10);
-          const sizeOfShip = array[ship];
-          let successfulLoopIterration = false;
-          for (let i = 0; i < sizeOfShip; i++) {
-            if (
-              y_axis - i < 0 ||
-              this.board[x_axis][y_axis - i].length !== 0 //this checks if all the cells, that are going to contain our ship, are valid
-            ) {
-              successfulLoopIterration = false;
-              break;
-            } else {
-              successfulLoopIterration = true;
-            }
-          }
-          if (successfulLoopIterration) {
-            for (let i = 0; i < sizeOfShip; i++) {
-              this.board[x_axis][y_axis - i] = ship;
-            }
-            randomiseIsDone = true;
-          }
+      } else {
+        for (let i = 0; i < array[ship]; i++) {
+          this.board[x_axis][y_axis - i] = ship;
         }
       }
     }
