c3d5cf4ad482b7591582cc54a341a230e610301b
StathisPlakakis <stathisplakorg1997@gmail.com>
2024-07-11 17:50:22 +0300

    Import classes into index.js and init PC/Player

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
 import './style.css';
+import Ship from './Classes/ship';
+import Gameboard from './Classes/gameboard';
+import Player from './Classes/player';
+
+const player = new Player(true);
+const computer = new Player(false);
