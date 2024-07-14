4e41dc10410dad6cfbd19c78aa147d0ec96a6514
StathisPlakakis <stathisplakorg1997@gmail.com>
2024-07-14 19:19:03 +0300

    Add click event that makes attack on specifi cell

--- src/index.js
+++ src/index.js
@@ -4,15 +4,14 @@ import displayGameboards from './DOM/gameboard';
 import randomizeBoard from './DOM/randomizeBoard';
 const realPlayer = new Player(true);
 const computerPlayer = new Player(false);
-const players = [realPlayer, computerPlayer];
 
-randomizeBoard(players);
-displayGameboards(players);
+randomizeBoard([realPlayer]);
+displayGameboards([realPlayer, computerPlayer]);
 
 const randomizeButton = document.querySelector('.randomize');
 const handlerandomizeButtonClick = () => {
-  randomizeBoard(players);
-  displayGameboards(players);
+  randomizeBoard([realPlayer]);
+  displayGameboards([realPlayer, computerPlayer]);
   randomizeButton.disabled = true;
   randomizeButton.style.cursor = 'default';
 
@@ -27,6 +26,8 @@ randomizeButton.addEventListener('click', handlerandomizeButtonClick);
 
 const startButton = document.querySelector('.start');
 const handlestartButtonClick = () => {
+  randomizeBoard([computerPlayer]);
+  displayGameboards([realPlayer, computerPlayer]);
   startButton.disabled = true;
   startButton.style.cursor = 'default';
   randomizeButton.disabled = true;
@@ -34,11 +35,17 @@ const handlestartButtonClick = () => {
   const cells = document.querySelectorAll('.computer td');
   cells.forEach((cell) => {
     cell.addEventListener('mouseenter', () => {
-      cell.style.backgroundColor = 'blue';
+      // cell.style.backgroundColor = 'blue';
       cell.style.cursor = 'crosshair';
     });
     cell.addEventListener('mouseleave', () => {
-      cell.style.backgroundColor = 'white';
+      // cell.style.backgroundColor = 'white';
+    });
+    cell.addEventListener('click', (e) => {
+      const specificCell = e.target;
+      const row = specificCell.getAttribute('row');
+      const column = specificCell.getAttribute('column');
+      computerPlayer.gameboard.receiveAttack([row, column]);
     });
   });
 };
